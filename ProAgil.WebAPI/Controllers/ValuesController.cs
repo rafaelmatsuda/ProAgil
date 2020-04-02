d8b394af8e31d78e2e0b1e65b80be06e11be08c5
Rafael Matsuda <rafael.matsuda@zenatur.com.br>
2020-04-02 16:15:23 -0300

    inicial

---
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebAPI.Data;
+using ProAgil.WebAPI.Model;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public  readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+
+        }
+
+
+        // GET: api/Values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);     
+            }
+            catch (System.Exception)
+            {   
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no banco de dados :(");
+            }
+        }
+
+        // GET: api/Values/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> Get(int id)
+        {
+           try
+           {
+               var results = await _context.Eventos.FirstOrDefaultAsync(e=> e.EventoId == id);
+               return Ok(results);
+           }
+           catch (System.Exception)
+           {
+               return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no banco de dados :(");
+           }
+        }
+
+        // POST: api/Values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
